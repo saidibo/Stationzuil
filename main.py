9eb7794a15ce8b07e246acdd25b0f65c8343cd92
SaidE <said.ebrahimin@student.hu.nl>
2022-09-29 11:51:33 +0200

    First commit Project Zuil readme file toegevoegd start van project zuil opdracht 1, ik heb alle inputs geschreven.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("dit is de STation zuil project")
+import datetime
+
+print("dit is de Station zuil project")
+
+
+
+msg_naam = str(input("Wat is u naam? "))
+msg = str(input("Beste klant, wat is u ervaring: "))
+today = datetime.datetime.now()
+date_time = today.strftime("%m/%d/%Y, %H:%M:%S ")
+
+info = msg_naam, msg, date_time
+
+if msg_naam == "":
+    msg_naam = "anoniem"
+elif msg_naam:
+    msg_naam == msg_naam
+
+if len(msg) > 140:
+    print("u bericht is te lang.")
+
+
+
+else:
+    None
+
+
+print(info)
+
+
