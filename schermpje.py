a8f474a6d70a4471bc6910e91836524372633a56
SaidE <said.ebrahimin@student.hu.nl>
2022-11-02 11:16:02 +0100

    First commit Project Zuil 2. main.py script geschreven 3. moderator script geschreven, met go en stop berichten 4. code is veel verder 5. tkinter

--- schermpje.py
+++ schermpje.py
@@ -1,17 +1,9 @@
-from tkinter import *
-root = Tk()
+import tkinter as tk
 #tkinter scherm importeren
 import psycopg2 as db
 #DB zuil inporteren voor zo nodig....
-# import requests
-# # import request
-# resource_uri = "http://api.openweathermap.org/geo/1.0/zip?zip={3085},{14}&appid={623674843eb5b413c214a36bad8eefdc}"
-# response = requests.get(resource_uri)
-# response_data = response.json()
-# for key in response_data.keys():
-#     print(f"{key}: {response_data[key]}")
-
-
+from weer import rek_api
+import json
 
 
 conn = db.connect(
@@ -32,20 +24,41 @@ for r in rij:
     info = (f'de heer {r[1]} zei: {r[2]}. in de plaats {r[3]}')
 
 
-
-label = Label(master=root,
-              text= info,
-              background='yellow',
-              foreground='blue',
-              font=('Arial', 16, 'bold italic'),
-              width=60,
-              height=20
-              )
-
-label.pack()
-
-root.mainloop()
+#
+#
+# label = Label(master=root,
+#               text= lst,
+#               background='yellow',
+#               foreground='blue',
+#               font=('Arial', 16, 'bold italic'),
+#               width=60,
+#               height=20
+#               )
+#
+# label.pack()
+#
+# root.mainloop()
 
 
 conn.commit()
 cur.close()
+
+root = tk.Tk()
+test = tk.Label(root,
+                text=info,
+                bg = 'yellow',
+                fg = 'black',
+                width=50,
+                height=10
+                )
+test.pack(ipadx=30, ipady=6)
+test = tk.Label(root,
+                text=rek_api(),
+                bg = 'yellow',
+                fg='black',
+                width=50,
+                height=10
+                )
+test.pack(ipadx=6, ipady=12)
+tk.mainloop()
+
---
+++ weer.py
@@ -0,0 +1,15 @@
+import requests
+
+# import request
+def rek_api():
+    resource_uri = "https://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=623674843eb5b413c214a36bad8eefdc"
+    response = requests.get(resource_uri)
+    response_data = response.json()
+    weer = response_data['main']
+    celcius = round(float(weer['temp'] - 273),2)
+    return str(celcius) + '°C'
+
+
+
+if __name__ == "__main__":
+    rek_api()
